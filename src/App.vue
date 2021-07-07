56bdbaef57f500ef8b42d48064bb4721fc1f591d
dindin-abdulloh <50541680+adin17-alt@users.noreply.github.com>
2021-07-07 17:25:16 +0700

    tampilan awal

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container" style="margin-top: 20px;">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">TODO APP</h5>
+        <div class="row">
+          <div class="col-10">
+            <input
+              v-model="todo"
+              type="text"
+              class="form-control"
+              @keyup.enter="add"
+            />
+          </div>
+          <div class="col-2">
+            <button class="btn btn-success" @click="add">ADD</button>
+          </div>
+        </div>
+        <list :todos="list" @deleteTodo="deleteTodo" @doneTodo="doneTodo" />
+        <br />
+        <small>Total TODO : {{ totalTODO }}</small>
+      </div>
+    </div>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref, reactive, toRefs, onMounted, computed } from "vue";
+import List from "./components/List.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  components: { List },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  setup() {
+    const todo = ref("");
+    const todos = reactive({
+      list: [],
+    });
+    onMounted(() => {
+      const items = localStorage.getItem("todos");
+      todos.list = items ? JSON.parse(items) : [];
+    });
+    const totalTODO = computed(() => {
+      return todos.list.length;
+    });
+    const add = () => {
+      todos.list.unshift({
+        activity: todo.value,
+        isDone: false,
+      });
+      todo.value = "";
+      saveToLocalStorage();
+    };
+    const deleteTodo = (todoIndex) => {
+      todos.list = todos.list.filter((item, index) => {
+        if (index != todoIndex) {
+          return item;
+        }
+      });
+      saveToLocalStorage();
+    };
+    const doneTodo = (todoIndex) => {
+      todos.list = todos.list.filter((item, index) => {
+        if (index == todoIndex) {
+          item.isDone = true;
+        }
+        return item;
+      });
+      saveToLocalStorage();
+    };
+    const saveToLocalStorage = () => {
+      localStorage.setItem("todos", JSON.stringify(todos.list));
+    };
+    return { todo, ...toRefs(todos), totalTODO, add, deleteTodo, doneTodo };
+  },
+};
+</script>
